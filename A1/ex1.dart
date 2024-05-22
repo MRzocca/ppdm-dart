67979560a40da57403d50d71abe82b1a9ec11cd6
Murilo Zocca <133894897+MRzocca@users.noreply.github.com>
2024-05-22 14:02:54 -0300

    Add files via upload

---
+++ A1/ex1.dart
@@ -0,0 +1,10 @@
+//1
+void main() {
+  var num = 10;
+
+  if (num > 0) {
+    print('O número $num é positivo.');
+  } else {
+    print('O número $num é negativo (ou nulo).');
+  }
+}
---
+++ A1/ex2.dart
@@ -0,0 +1,6 @@
+void main() {
+  var num2 = 20;
+
+  var mensagem =
+      num2 % 2 == 0 ? ' número $num2 é par.' : 'O número $num2 é impar';
+}
